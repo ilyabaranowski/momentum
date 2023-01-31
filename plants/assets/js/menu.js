f9d6d2dee3be6a74c547c0e956ac47c38522bbcd
ilyabaranowski <iliuxa13@ya.ru>
2023-01-31 21:23:00 +0300

    refactor

--- plants/assets/js/menu.js
+++ plants/assets/js/menu.js
@@ -1,21 +1,21 @@
 const open = document.querySelector('.toggle-nav');
 const close = document.querySelector('.close-icon');
 const menu = document.querySelector('.mobile-nav');
-const dark = document.querySelector('.dark');
+const dark =document.querySelector('.dark');
 
-open.onclick = function() {
+open.onclick = function () {
     menu.style.transform = 'translateX(0)';
-    dark.classList.add("active");
+    dark.classList.add('active');
 };
 
-close.onclick = function() {
+close.onclick = function () {
     menu.style.transform = 'translateX(100%)';
-    dark.classList.remove("active");
+    dark.classList.remove('active');
 };
 
 document.onclick = function (e) {
-    if (e.target.className != "mobile-nav" && e.target.className != 'toggle-nav' ) {
+    if (e.target.className != 'mobile-nav' && e.target.className != 'toggle-nav' ) {
         menu.style.transform = 'translateX(100%)';
-        dark.classList.remove("active");
+        dark.classList.remove('active');
     };
 };
