a52a45698f9df66eab3331735f0a0a80f8ea22c5
ilyabaranowski <iliuxa13@ya.ru>
2023-02-22 00:18:29 +0300

    refactor:date function

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,12 +1,10 @@
 const time = document.querySelector('.time');
 const dateInfo = document.querySelector('.date');
-const date = new Date();
-const hours = date.getHours();
 
-///////////////////Time
+///////////////////Time & date
 function showTime() {
     const date = new Date();
-    const currentTime = date.toLocaleTimeString("en-US", { hour12: false });
+    const currentTime = date.toLocaleTimeString('en-US', { hour12: false });
     time.textContent = currentTime;
 
     function showDate(){
@@ -15,10 +13,14 @@ function showTime() {
         month: 'long',
         day:'numeric',
     };
-    const currentDate = date.toDateString('en-US', options);
+    const currentDate = date.toLocaleDateString('en-US', options);
     dateInfo.textContent = currentDate;
     }
     showDate();
     setTimeout(showTime, 1000);
 }
 showTime();
+
+//////////////Greeting
+
+
